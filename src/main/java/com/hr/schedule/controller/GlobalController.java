ced8b19658652c8c7e817942c07459ae64ed7b42
funsaF <86002626+funsaF@users.noreply.github.com>
2021-08-28 22:15:22 +0800

    根據權限隱藏按鈕pages

--- src/main/java/com/hr/schedule/controller/GlobalController.java
+++ src/main/java/com/hr/schedule/controller/GlobalController.java
@@ -1,10 +1,13 @@
 package com.hr.schedule.controller;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.hr.leave.model.ListBean;
 import com.hr.leave.service.LeaveService;
+import com.hr.login.model.Authorities;
+import com.hr.login.model.LoginModel;
 import com.hr.login.service.LoginService;
 
 @Controller
@@ -30,4 +35,27 @@ public class GlobalController {
 	public @ResponseBody List<ListBean> findListByCategory(@RequestParam("category") String category) {
 		return leaveService.findListByCategory(category);
 	}
+
+	@GetMapping(value="/findAUTHByEmpNo",produces= {"application/json; charset=UTF-8"})
+	public @ResponseBody boolean isAuthMoreThanGEN(@ModelAttribute("loginModel") LoginModel loginModel) {
+		  LoginModel loginM = loginService.getLoginModelByEmpNo(loginModel.getEmpNo());
+		  
+		  Set<Authorities> set = loginM.getAuthorities();
+//		  Iterator<Authorities> iterator = set.iterator();
+//		  boolean check = false;
+		  boolean moreThanGen = false;
+		  System.out.println(set.size());
+		  if(set.size() > 1) {
+			  moreThanGen = true;
+		  }
+//		  while(iterator.hasNext()){
+//		      String s = iterator.next().getAuthorityName();
+//		      System.out.println(s.toString());
+//		      if(s.equals("ROLE_GENERAL")) {
+//		       check = true;
+//		      }
+//		  }
+		  return moreThanGen;
+	}
+	
 }
