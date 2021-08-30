9a0481be4d56029af28dab3185acc1b3290b0f83
Angela <angela-0326@yahoo.com.tw>
2021-08-31 00:05:44 +0800

    還原logincontroller

--- src/main/java/com/hr/login/controller/LoginController.java
+++ src/main/java/com/hr/login/controller/LoginController.java
@@ -3,8 +3,6 @@ package com.hr.login.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,10 +10,6 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.hr.login.model.LoginModel;
@@ -27,17 +21,17 @@ public class LoginController {
 	
 	@Autowired
 	private LoginService loginService;
-	
-	@GetMapping(path="/")
-	public String mainPagePath() {
-		return "redirect:/index";
-	}
-	
+
 	@GetMapping(path="/login")
 	public String loginPagePath() {
 		return "/login";
 	}
 	
+  @GetMapping(path="/")
+	public String mainPagePath() {
+		return "redirect:/index";
+	}
+  
 	@GetMapping(path="/index")
 	public String redirectToMainPage(Model model) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -46,7 +40,7 @@ public class LoginController {
 		model.addAttribute("loginModel", loginModel);
 		return "/index";
 	}
-	
+
 	@GetMapping(path="/logout")
 	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
 	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -55,9 +49,10 @@ public class LoginController {
 	    }
 	    return "/login";
 	}
-	
-	@GetMapping(path="/test")
-	public String test() {
-		return "/test";
-	}
+
+	@GetMapping(path="/pages")
+	public String loginPage() {
+		return "/pages";
+
+	}	
 }
